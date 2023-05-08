1e9026ffde3f89a8bccf92f2592879fadc95a04d
18OJohnston.ACC <18ojohnston.acc@lwetb.ie>
2023-05-08 11:35:27 +0100

    added some comments

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 addEventListener("DOMContentLoaded", (e) => {
+	// Ensure that only numbers can be typed into the text-box
 	let cc_num = document.getElementById('cc_num')
 	cc_num.addEventListener("keypress", (e) => {
 		code = (e.keyCode ? e.keyCode : e.which);
-		if (code >= 48 && code <=57) {
+		if (code >= 48 && code <=57) { // unicode range for [0-9]
 			return
-		}
+		} 
 		e.preventDefault();
 	});
 
@@ -14,14 +15,18 @@ addEventListener("DOMContentLoaded", (e) => {
 
 
 function verify_number() {
-	var numbers = cc_num.value.split('').map((i) => Number(i));
-	var last_num = numbers.splice(numbers.length-1, 1);
-	numbers = numbers.reverse()
+	// we can assume that every value is a number as we've already sanitized the input
+	var numbers = cc_num.value.split('').map((i) => Number(i)); // Convert the string into a list of numbers
+	
+	var last_num = numbers.splice(numbers.length-1, 1); // remove the last number and store it in a variable
+	
+	numbers = numbers.reverse() // reverse the remaining numbers
+	
 	var check_number = 0
-	numbers.filter((_, i) => !Boolean(i&1)).forEach((value, i) => {
+	numbers.filter((_, i) => !Boolean(i&1)).forEach((value, i) => { // iterate over all the even numbers
 		var n = value*2;
 		if (n>9) {n -= 9}
-		check_number += n;
+		check_number += n; // add it to the check string
 	});
 	check_number += last_num;
 	if (check_number%10==0) {
